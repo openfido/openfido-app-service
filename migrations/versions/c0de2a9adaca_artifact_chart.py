5c56ae2e7dc06ff587ad895b3b1c24db65d02f89
Thistleman <codes5096@gmail.com>
2022-05-24 16:01:40 -0700

    Properly updated the alembic migration scripts

--- migrations/versions/c0de2a9adaca_artifact_chart.py
+++ migrations/versions/c0de2a9adaca_artifact_chart.py
@@ -7,6 +7,8 @@ Create Date: 2020-11-09 00:28:59.033914
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.engine.reflection import Inspector
+
 
 
 # revision identifiers, used by Alembic.
@@ -15,9 +17,33 @@ down_revision = 'a0e54652a189'
 branch_labels = None
 depends_on = None
 
+conn = op.get_bind()
+inspector = Inspector.from_engine(conn)
+tables = inspector.get_table_names()
 
 def upgrade():
-    pass
+    if table_name not in tables:
+        op.create_table(
+            "artifact_chart",
+            sa.Column("id", sa.Integer(), nullable=False),
+            sa.Column("uuid", sa.String(length=32), server_default="", nullable=False),
+            sa.Column("name", sa.String(length=128), nullable=False),
+            sa.Column("organization_pipeline_run_id", sa.Integer(), nullable=False),
+            sa.Column(
+                "artifact_uuid", sa.String(length=32), server_default="", nullable=False
+            ),
+            sa.Column("chart_type_code", sa.String(length=20), nullable=False),
+            sa.Column("chart_config", sa.JSON(), nullable=True),
+            sa.Column("created_at", sa.DateTime(), nullable=False),
+            sa.Column("updated_at", sa.DateTime(), nullable=False),
+
+
+            sa.PrimaryKeyConstraint("id"),
+            sa.ForeignKeyConstraint(
+                ["organization_pipeline_run_id"],
+                ["organization_pipeline_run.id"],
+            ),
+        )
 
 
 def downgrade():
